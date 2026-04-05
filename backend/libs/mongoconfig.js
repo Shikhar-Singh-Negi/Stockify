29b3717c7feda241cd673dc8c8923ebf11180446
Shikhar <s.snegi1404@gmail.com>
2026-04-05 17:09:37 +0530

    Some Updates

--- backend/libs/mongoconfig.js
+++ backend/libs/mongoconfig.js
@@ -4,7 +4,7 @@ const mongoose=require("mongoose")
 require("dotenv").config()
 
 module.exports.MongoDBconfig=()=>{
-    mongoose.connect(process.env.MONGODB_URL)
+    mongoose.connect(process.env.ATLASDB_URL || process.env.MONGODB_URL)
     .then(()=>{
         console.log("connected to database successfully")
     })
